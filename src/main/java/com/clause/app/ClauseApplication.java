1553126e96b18f5ac7308e71ba6ad63d4c95e94a
luna <lundreame34@gmail.com>
2025-12-19 02:08:30 +0900

    first commit

---
+++ src/main/java/com/clause/app/ClauseApplication.java
@@ -0,0 +1,13 @@
+package com.clause.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClauseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClauseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clause/app/ClauseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clause.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClauseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
